370193c38151543d676a9311327cb296e1445e22
Arjun <kingarjun74280@gmail.com>
2026-04-29 20:34:59 +0530

    Intialize the project and make landing page

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+        <div className='flex'>
+            <div className='bg-red-400 w-2/4'></div>
+            <div className='bg-red-600'></div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,15 @@
+import HeroSection from '@/components/HeroSection'
+import NavbarDemo from '@/components/resizable-navbar-demo'
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <NavbarDemo/>
+        <HeroSection/>
+        
+        LandingPage</div>
+  )
+}
+
+export default LandingPage
