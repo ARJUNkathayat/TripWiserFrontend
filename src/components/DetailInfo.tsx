877e2c1b66e111fee0c9ac8fcbce97f7d143fc08
Arjun <kingarjun74280@gmail.com>
2026-05-13 21:52:13 +0530

    ..

--- src/components/DetailInfo.tsx
+++ src/components/DetailInfo.tsx
@@ -5,9 +5,21 @@ const DetailInfo = () => {
   return (
     <div>
         {/* first div */}
-        <div>
-            <div></div>
-            <div></div>
+        <div className='flex'>
+            <div>
+                            <div className='w-[40%] bg-amber-500'> 
+                                <h1>Bali,Indonesia</h1>
+                                <h3>An immersive island Lorem ipsum dolor sit amet consectetur <br></br>adipisicing elit. Tenetur, nihil magni, ullam aspernatur, quidem non officia impedit saepe facilis unde cumque possimus minima expedit
+                                    a earum sit hic esse sint quibusdam.</h3>
+                                
+                                 </div>
+                            <div className='w-[60%] bg-amber-300'></div>
+            </div>
+
+            <div>
+
+            </div>
+
         </div>
         {/* second div */}
         <div></div>
