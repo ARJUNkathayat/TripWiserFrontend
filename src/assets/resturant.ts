b24163849e19692ef725229d1dbb9b46d686d560
Arjun <kingarjun74280@gmail.com>
2026-05-15 21:28:28 +0530

    Add resturant data and make card for it

---
+++ src/assets/resturant.ts
@@ -0,0 +1,272 @@
+export const resturants =[
+    {
+        "name": "Rumours",
+        "rating": 4.4,
+        "reviewCount": 18,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/e3/75/0b/outdoor-sitting.jpg?w={width}&h={height}&s=1",
+        "address": "Euphoria shopping complex 5th floor above Hamleys, Dehradun 248009 India",
+        "cuisines": ["Italian", "Chinese", "Indian", "Dining bars", "Beer restaurants"],
+        "priceLevel": "₹₹₹₹"
+    },
+    {
+        "name": "Auric Restaurant & Rooftop Café",
+        "rating": 4.4,
+        "reviewCount": 12,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/31/0d/10/0a/this-picture-encapsulates.jpg?w={width}&h={height}&s=1",
+        "address": "179/2, Rajpur Road Near Mussoorie Diversion, Dehradun 248009 India",
+        "cuisines": ["Italian", "Mexican", "Chinese", "Indian", "Bar", "Asian"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "The 3 Pines Cafe",
+        "rating": 4.8,
+        "reviewCount": 22,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/bf/d1/ca/bells-bottles-of-love.jpg?w={width}&h={height}&s=1",
+        "address": "180 A, Rajpur Road Near Sai Temple, Dehradun 248001 India",
+        "cuisines": ["Italian", "Mexican", "Chinese", "Cafe", "Grill"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "First Gear Cafe",
+        "rating": 4.1,
+        "reviewCount": 72,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/9b/5b/72/first-gear-cafe.jpg?w={width}&h={height}&s=1",
+        "address": "Mussoorie Road, Dehradun 248009 India",
+        "cuisines": ["Cafe", "Fast food"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "Kalsang Restaurant",
+        "rating": 4.1,
+        "reviewCount": 29,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/9c/84/83/wanna-try-some-chinese.jpg?w={width}&h={height}&s=1",
+        "address": "88 A, Rajpur Rd, Opposite Osho, Chander Lok Colony, Hathibarkala Salwala, Dehradun 248001 India",
+        "cuisines": ["Chinese", "Asian", "Thai", "Korean", "Tibetan"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "Pyramid Dehradun",
+        "rating": 4.4,
+        "reviewCount": 7,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2d/37/6d/7d/perfect-ambiance.jpg?w={width}&h={height}&s=1",
+        "address": "Khasra No 15, Rajpur Road Near Mussoorie Diversion, Dehradun 248001 India",
+        "cuisines": ["Italian", "Lebanese", "Bar", "Cafe", "Mediterranean", "Asian", "Pub", "Wine Bar", "Indonesian", "Dining bars", "Beer restaurants"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "Doon Darbar",
+        "rating": 3.9,
+        "reviewCount": 69,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/83/a2/52/doon-darbar.jpg?w={width}&h={height}&s=1",
+        "address": "43 Gandhi Road Near Clock Tower, Dehradun 248001 India",
+        "cuisines": ["Indian", "Asian"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "The Malt Bar",
+        "rating": 4.9,
+        "reviewCount": 7,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2b/1b/b0/6b/caption.jpg?w={width}&h={height}&s=1",
+        "address": "Hyatt Regency Dehradun , Dehradun Mussoorie Road Near Ram Tirath Ashram, Kuthal Gate, Dehradun 248009 India",
+        "cuisines": ["Indian", "Bar", "Grill", "Pub", "Indigenous"],
+        "priceLevel": null
+    },
+    {
+        "name": "Kumar Foods Restaurant",
+        "rating": 4.0,
+        "reviewCount": 178,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/05/37/5c/b5/kumar-resaurant.jpg?w={width}&h={height}&s=1",
+        "address": "15B, Rajpur Road, Dehradun 248001 India",
+        "cuisines": ["Indian", "Asian"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "The Swings Cafe (A Family Restaurant & Bar)",
+        "rating": 4.7,
+        "reviewCount": 39,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/c7/7b/c6/cafe-by-night.jpg?w={width}&h={height}&s=1",
+        "address": "Brothers Tower, 163 A, 1st Floor, Rajpur Rd, Kishanpur, Dehradun, Uttarakhand next to Royal Enfield Showroom, Dehradun 248001 India",
+        "cuisines": ["Indian", "Bar", "Pub"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "Danke",
+        "rating": 5.0,
+        "reviewCount": 4,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/32/e3/94/a0/caption.jpg?w={width}&h={height}&s=1",
+        "address": "180 Rajpur Road Near Sai Mandir, Dehradun 248009 India",
+        "cuisines": [],
+        "priceLevel": null
+    },
+    {
+        "name": "Anandam",
+        "rating": 3.9,
+        "reviewCount": 173,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/58/1d/4a/photo1jpg.jpg?w={width}&h={height}&s=1",
+        "address": "69 Rajpur Road, Dehradun 248001 India",
+        "cuisines": ["Indian"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "Lhasa Tibet Kitchen",
+        "rating": 4.2,
+        "reviewCount": 33,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/42/42/4b/lhasa-tibet-kitchen.jpg?w={width}&h={height}&s=1",
+        "address": "Sahastradhara Road Near Usha Colony, Dehradun 248013 India",
+        "cuisines": ["Chinese", "Asian", "Tibetan"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "Food Chetan Puri Wala",
+        "rating": 4.5,
+        "reviewCount": 14,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2e/f0/09/29/caption.jpg?w={width}&h={height}&s=1",
+        "address": "5, Paltan Bazar Rd Opposite Railway Station entry road, Dehradun 248001 India",
+        "cuisines": ["Indian"],
+        "priceLevel": "₹"
+    },
+    {
+        "name": "Musee Art Cafe",
+        "rating": 4.1,
+        "reviewCount": 11,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/35/f7/bf/outdoor-garden-area.jpg?w={width}&h={height}&s=1",
+        "address": "Nagal Rd, Kishanpur, Dehradun, Uttarakhand 248009, Dehradun 248009 India",
+        "cuisines": ["Italian", "American", "Indian", "Cafe", "Asian"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "Cafe Marigold",
+        "rating": 4.0,
+        "reviewCount": 55,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2f/4d/5a/84/cafe-marigold.jpg?w={width}&h={height}&s=1",
+        "address": "Old Mussorie Road Near Shehanshahi Ashram, Dehradun 248009 India",
+        "cuisines": ["Cafe"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "Punjab Grill",
+        "rating": 4.2,
+        "reviewCount": 77,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1d/9d/4d/cf/ambience.jpg?w={width}&h={height}&s=1",
+        "address": "Jakhan, Rajpur Road, Dehradun 248001 India",
+        "cuisines": ["Indian", "Asian"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "Kaafal Restaurent",
+        "rating": 4.7,
+        "reviewCount": 10,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/8b/fa/3c/kaafal-inside-view.jpg?w={width}&h={height}&s=1",
+        "address": "Sahastradhara Road Near It Park, Dehradun 248001 India",
+        "cuisines": [],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "Cafe Cibo",
+        "rating": 4.2,
+        "reviewCount": 47,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/e1/2f/da/cafe-cibo.jpg?w={width}&h={height}&s=1",
+        "address": "44.B.East Canal Road Near Dwarka Stores, Dehradun 248001 India",
+        "cuisines": ["Italian", "Cafe"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "Chili's American Grill & Bar",
+        "rating": 4.9,
+        "reviewCount": 16,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/31/a9/e9/d1/caption.jpg?w={width}&h={height}&s=1",
+        "address": "Ground floor,Pacific Mall,Jakhan,Rajpur Road Opp.Shcolars home,SH/GF/04, Dehradun 248001 India",
+        "cuisines": ["Mexican", "American"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "Uss Da Dhabha",
+        "rating": 4.1,
+        "reviewCount": 20,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/1a/05/e6/nice-food.jpg?w={width}&h={height}&s=1",
+        "address": "3 Court Road near Saurav Motor, Dehradun 248001 India",
+        "cuisines": ["Indian", "Asian"],
+        "priceLevel": "₹"
+    },
+    {
+        "name": "The Bend",
+        "rating": 4.7,
+        "reviewCount": 7,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/28/7a/5c/eb/a-look-at-our-exteriors.jpg?w={width}&h={height}&s=1",
+        "address": "92/1, Mussoorie Bypass Road, Dehradun 248009 India",
+        "cuisines": ["American", "Indian", "European", "Asian", "Diner", "Central European"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "Aura Kitchen And Spirits",
+        "rating": 4.3,
+        "reviewCount": 7,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/5e/50/45/dine-inn-hall.jpg?w={width}&h={height}&s=1",
+        "address": "1064/2, General Mahadev Singh Rd, Vyomprasth, Kanwali, Dehradun 248001 India",
+        "cuisines": ["Chinese", "Indian"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "Zeiv Hermitage Yoga & Wellness Resort",
+        "rating": 5.0,
+        "reviewCount": 1,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2d/73/3f/2e/caption.jpg?w={width}&h={height}&s=1",
+        "address": "Theva Maldevta Road, Dehradun 248008 India",
+        "cuisines": ["Chinese", "Indian", "Fast food", "Asian", "Bangladeshi"],
+        "priceLevel": "₹"
+    },
+    {
+        "name": "Malabar Restaurant",
+        "rating": 4.8,
+        "reviewCount": 20,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/be/9a/7e/chicken-changezi.jpg?w={width}&h={height}&s=1",
+        "address": "193, 1, Rajpur Rd Near Sai Mandir, Dehradun 248009 India",
+        "cuisines": ["Chinese", "Indian", "Cafe", "Soups"],
+        "priceLevel": "₹"
+    },
+    {
+        "name": "Tavern Bar and Restaurant",
+        "rating": 4.2,
+        "reviewCount": 34,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/a0/c8/ca/decorations-during-ipl.jpg?w={width}&h={height}&s=1",
+        "address": "2nd floor,Cross Road Mall, EC Road, Chironwali opposite Nany's Bakery, Dehradun 248001 India",
+        "cuisines": ["Chinese", "Indian", "Bar", "Pub"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    },
+    {
+        "name": "YETI - The Himalyan Kitchen Dehradun",
+        "rating": 4.8,
+        "reviewCount": 4,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/d1/c1/3f/caption.jpg?w={width}&h={height}&s=1",
+        "address": "170,Rajpur Road,Kishanpur Next to Starbucks, Dehradun 248001 India",
+        "cuisines": ["Chinese", "Asian", "Tibetan", "Nepalese"],
+        "priceLevel": null
+    },
+    {
+        "name": "Cafe Sparrow",
+        "rating": 4.2,
+        "reviewCount": 6,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/62/4f/69/the-sitting-place-in.jpg?w={width}&h={height}&s=1",
+        "address": "Sherki Maldevta Near Aranya Retreat, Dehradun 248008 India",
+        "cuisines": ["American", "Indian", "Cafe", "Street Food", "Israeli"],
+        "priceLevel": null
+    },
+    {
+        "name": "Chica Dehradun",
+        "rating": 5.0,
+        "reviewCount": 2,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/33/1c/75/8b/elevated-above-the-city.jpg?w={width}&h={height}&s=1",
+        "address": "Pranav Tower, 210, Rajpur Rd, opposite GRD, IAS Officers Colony, Rajpur, Dehradun 248009 India",
+        "cuisines": ["Indian", "Bar", "Mediterranean", "European", "Asian"],
+        "priceLevel": null
+    },
+    {
+        "name": "House of Nuttmeeg",
+        "rating": 5.0,
+        "reviewCount": 1,
+        "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/32/7f/f7/6f/house-of-nuttmeeg-aesthetic.jpg?w={width}&h={height}&s=1",
+        "address": "St.Joseph School, Rajpur Rd Ashtley Hall, Irigation Colony, Dehradun 248001 India",
+        "cuisines": ["Indian"],
+        "priceLevel": "₹₹ - ₹₹₹"
+    }
+]
